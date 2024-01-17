b32abd81abc562eb1f2785cabc6854e06c61a016
Error-2515 <abdullah.h.shaikh03@gmail.com>
2024-01-17 23:17:38 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed=new Typed('.typing',{
+    strings:["Artificial Intelligence And Data Student","Web Developer","App developer","python enthusiast"],
+    typeSpeed:80,
+    backSpeed:100,
+    loop:true
+})
